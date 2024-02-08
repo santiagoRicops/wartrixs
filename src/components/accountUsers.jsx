25014062c10a7c5cf09257a69b3f3b1a4b4c8aef
santiagoRicops <santiagoricopassops@gmail.com>
2024-02-07 22:24:40 -0500

    🚀

--- src/components/accountUsers.jsx
+++ src/components/accountUsers.jsx
@@ -29,6 +29,7 @@ const LoginForm = ({ className }) => {
           id="Name"
           className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
           placeholder="Name"
+          
         />
       </div>
       <div className="mb-4">
