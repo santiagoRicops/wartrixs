ec5a993cfb7f7cc9b5f566731ed25728c60c2d52
santiagoRicops <santuago231@gmail.com>
2024-02-20 22:19:18 -0500

    🚀 changes are being made to the ui

--- src/components/homeCaruselProducts.jsx
+++ src/components/homeCaruselProducts.jsx
@@ -8,7 +8,9 @@ import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 const HomeCaruselProducts = () => {
   const [products, setProducts] = useState([])
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({delay: 4000})])
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 4000 }),
+  ])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,23 +33,27 @@ const HomeCaruselProducts = () => {
     if (emblaApi) emblaApi.scrollNext()
   }, [emblaApi])
   return (
-<>
-  <section className=' justify-center items-center relative w-[100%]'>
-  <button className="embla__prev w-[100px] h-[50px] m-[10px] bg-slate-600" onClick={scrollPrev}>
-      Prev
-    </button>
-    <button className="embla__next w-[100px] h-[50px] m-[10px] bg-slate-600" onClick={scrollNext}>
-      Next
-    </button>
-    <div ref={emblaRef} className='embla__viewport'>
-      <div className='embla__container flex p-[100px]'>
-        <CardProduct products={products} />
-      </div>
-    </div>
-   
-  </section>
-</>
-
+    <>
+      <section className=" justify-center items-center relative w-[100%]">
+        <button
+          className="embla__prev w-[100px] h-[50px] m-[10px] bg-slate-600"
+          onClick={scrollPrev}
+        >
+          Prev
+        </button>
+        <button
+          className="embla__next w-[100px] h-[50px] m-[10px] bg-slate-600"
+          onClick={scrollNext}
+        >
+          Next
+        </button>
+        <div ref={emblaRef} className="embla__viewport">
+          <div className="embla__container flex p-[100px]">
+            <CardProduct products={products} />
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
