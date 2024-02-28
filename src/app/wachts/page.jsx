5255a821bf06c10dcb636687bfddcd23fd4eb2c5
santiagoRicops <santuago231@gmail.com>
2024-02-27 21:35:05 -0500

    🐛

--- src/app/wachts/page.jsx
+++ src/app/wachts/page.jsx
@@ -2,14 +2,12 @@ import ProductContainer from '@/components/productsContianer'
 
 import NavBar from '@/components/navBar'
 
-
 const Relojes = () => {
   return (
     <>
       <NavBar />
 
       <ProductContainer filterProductsPage={'wachts'} />
-     
     </>
   )
 }
