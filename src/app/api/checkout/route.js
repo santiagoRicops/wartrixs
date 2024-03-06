398015440e0a9c6d7d01ccbcabad25a7b8b7ada5
santiagoRicops <santuago231@gmail.com>
2024-03-05 22:23:26 -0500

    🚀

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,15 +1,17 @@
-import paypal from '@paypal/checkout-server-sdk';
-import { NextResponse } from 'next/server';
+import paypal from '@paypal/checkout-server-sdk'
+import { NextResponse } from 'next/server'
 
-const clientId = 'AbOZleQ5SYIOnwsvxuycf1IFqz8K8ov3nhNE41KfeI0PuzZ_GryLB45B8MERzah3GxGvPmqlx2rLz3SE';
-const clientSecret = 'EIqQaq_iGd9yPrezdpW8RUZ_4br3Te2BjAcvwdrFNoMnG6wtbWbyb6B_2A7c-Sqyscz10l8bivhgpN2m';
+const clientId =
+  'AbOZleQ5SYIOnwsvxuycf1IFqz8K8ov3nhNE41KfeI0PuzZ_GryLB45B8MERzah3GxGvPmqlx2rLz3SE'
+const clientSecret =
+  'EIqQaq_iGd9yPrezdpW8RUZ_4br3Te2BjAcvwdrFNoMnG6wtbWbyb6B_2A7c-Sqyscz10l8bivhgpN2m'
 
-const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
-const client = new paypal.core.PayPalHttpClient(environment);
+const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret)
+const client = new paypal.core.PayPalHttpClient(environment)
 
 export async function POST() {
   try {
-    const request = new paypal.orders.OrdersCreateRequest();
+    const request = new paypal.orders.OrdersCreateRequest()
     request.requestBody({
       intent: 'CAPTURE',
       purchase_units: [
@@ -46,21 +48,21 @@ export async function POST() {
           ],
         },
       ],
-    });
+    })
 
-    const response = await client.execute(request);
+    const response = await client.execute(request)
 
     // Devuelve la respuesta en formato JSON
     return NextResponse.json({
       id: response.result.id,
-    });
+    })
   } catch (error) {
-    console.error('Error al crear la orden:', error.message);
+    console.error('Error al crear la orden:', error.message)
 
     // Devuelve una respuesta de error en formato JSON
     return NextResponse.json({
       error: 'Error al procesar el pago',
       message: error.message,
-    });
+    })
   }
 }
