f774b21f3721cecfaa37b741c187493e1fe7592f
santiagoRicops <santuago231@gmail.com>
2024-03-06 22:12:09 -0500

    🐛

--- context/Account.jsx
+++ context/Account.jsx
@@ -1,42 +1,50 @@
 'use client'
 // context/Account.js
-import { createContext, useEffect, useState } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '../firebase'
+import { createContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
 
-export const AccountContext = createContext()
+export const AccountContext = createContext();
 
 export const AccountProvider = ({ children }) => {
-  const [myData, setMyData] = useState('')
-  const [sendAddress, setSendAddress] = useState('')
-  const [userData, setUserData] = useState('')
+  const [myData, setMyData] = useState('');
+  const [sendAddress, setSendAddress] = useState('');
+  const [userData, setUserData] = useState('');
 
   useEffect(() => {
     const localData = onAuthStateChanged(auth, (user) => {
       if (user) {
-        const storedData = localStorage.getItem('nameUser')
-        const sendAddress = localStorage.getItem('sendAddress')
+        // Verifica si localStorage está disponible antes de usarlo
+        if (typeof localStorage !== 'undefined') {
+          const storedData = localStorage.getItem('nameUser');
+          const sendAddress = localStorage.getItem('sendAddress');
 
-        setMyData(storedData)
-        setUserData(user)
-        setSendAddress(sendAddress)
+          setMyData(storedData);
+          setUserData(user);
+          setSendAddress(sendAddress);
+        }
       }
-    })
+    });
 
-    return () => localData()
-  }, [])
+    return () => localData();
+  }, []);
 
   const SignOff = async () => {
     try {
-      await auth.signOut()
-      //   localStorage.removeItem('nameUser')
-      setUserData(null)
+      await auth.signOut();
+      // Verifica si localStorage está disponible antes de usarlo
+      if (typeof localStorage !== 'undefined') {
+        localStorage.removeItem('nameUser');
+      }
+      setUserData(null);
     } catch (error) {}
-  }
-  console.log(sendAddress)
+  };
+
+  console.log(sendAddress);
+
   return (
     <AccountContext.Provider value={{ userData, myData, SignOff, sendAddress }}>
       {children}
     </AccountContext.Provider>
-  )
-}
+  );
+};
